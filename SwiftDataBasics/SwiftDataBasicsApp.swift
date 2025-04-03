36a03bf27474e04879a32d87d60826ff34feb591
CharlesMich <charleedongo@hotmail.com>
2025-04-03 12:56:02 -0500

    Initial Commit

---
+++ SwiftDataBasics/SwiftDataBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataBasicsApp.swift
+//  SwiftDataBasics
+//
+//  Created by Charles Michael on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
